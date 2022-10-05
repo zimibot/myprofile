ee57e560cb26f7859cb9e0e1e12b26edaee79656
zimibot <zimibot@gmail.com>
2022-10-05 14:45:50 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,11 @@
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/autoplay';
+import 'swiper/css/effect-cards';
+import 'swiper/css/effect-fade';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
